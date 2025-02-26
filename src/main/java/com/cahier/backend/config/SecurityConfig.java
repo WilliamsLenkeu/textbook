89c12696aa8def1206714125a44dcc922ddbd570
Wally🤖 <williams25prince@gmail.com>
2025-02-26 18:47:09 +0100

    ajout du chemin de test au exclusion pour le jeton jwt

--- src/main/java/com/cahier/backend/config/SecurityConfig.java
+++ src/main/java/com/cahier/backend/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,7 +40,8 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable()) // Désactive CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll() // Autorise les routes d'authentification
+                        .requestMatchers("/test/**").permitAll() // Autorise les routes de test
                         .anyRequest().authenticated() // Toute autre requête doit être authentifiée
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Désactive les sessions
