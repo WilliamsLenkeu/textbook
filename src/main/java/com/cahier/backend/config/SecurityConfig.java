c234600cb7e9e08e8010899b59e568c36def4901
Wally🤖 <williams25prince@gmail.com>
2025-03-05 16:50:40 +0100

    fix

--- src/main/java/com/cahier/backend/config/SecurityConfig.java
+++ src/main/java/com/cahier/backend/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.cahier.backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,29 +14,39 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtFilter jwtFilter; // Le filtre pour gérer le token JWT
     private final UserDetailsService userDetailsService;
 
-    // Définition du bean PasswordEncoder
+    @Bean
+    public AuthenticationManager authManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Utilisation de BCrypt pour encoder les mots de passe
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable) // Désactive CSRF si nécessaire (si tu n'utilises pas de formulaire)
+                .csrf(AbstractHttpConfigurer::disable) // Désactive CSRF si nécessaire
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // Permet l'accès à toutes les routes
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) // Garde les sessions actives
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) // Ajoute le filtre JWT pour gérer l'authentification par token
                 .build();
     }
 }
